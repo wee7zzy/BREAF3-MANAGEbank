27a154517fbf6b5e7ff144194e34038893c7f8fd
wee7zzy <yassaniq@gmail.com>
2024-12-26 15:43:29 +0100

    Creation du menu client et la fonction ajouter avec les expressions regex pour entrer l'email et le numero telephone comme il faut

--- src/Client.java
+++ src/Client.java
@@ -9,7 +9,10 @@ public class Client {
     private String nom, prenom, email, adresse;
     private String telephone;
 
-    public static ArrayList<Client> clients = new ArrayList<>();
+    String regexEmail="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    String regexTelephone="^(?:6|7|5)[0-9]{8}$";
+
+    public ArrayList<Client> clients = new ArrayList<>();
 
     public Client(int id, String nom, String prenom, String email, String adresse, String telephone) {
         this.id = id;
@@ -21,9 +24,7 @@ public class Client {
     }
 
     public Client() {
-
     }
-
 
     public int getId() {
         return id;
@@ -71,5 +72,84 @@ public class Client {
 
     public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+
+    public  void ajouterclient(){
+        try {
+            System.out.print("Entrez id : ");
+            this.setId(scanner.nextInt());
+            for(Client client:clients){
+                if(client.getId()==this.getId()){
+                    System.out.println("ID EXISTE DEJA ENTRER NOUVELLE ID :");
+                    this.setId(scanner.nextInt());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erreur : Entrée invalide !");
+        }
+
+        System.out.print("entrer nom :");
+        this.setNom(scanner.next());
+        scanner.nextLine();
+        System.out.print("entrer prenom :");
+        this.setPrenom(scanner.nextLine());
+        System.out.print("entrer email :");
+        this.email=scanner.next();
+        scanner.nextLine();
+        if (!email.matches(regexEmail)) {
+            do {
+                System.out.println("email invalid entrer EMAIL VALIDE :");
+                email = scanner.nextLine();
+            } while (!email.matches(regexEmail));
+        }
+        System.out.print("entrer adresse :");
+        this.setAdresse(scanner.nextLine());
+        System.out.print("entrer telephone : (+212)");
+        this.telephone=scanner.nextLine();
+
+        if (!telephone.matches(regexTelephone)) {
+            do {
+                System.out.println("telephone invalid entrer numero VALIDE :");
+                telephone = scanner.nextLine();
+            } while (!telephone.matches(regexTelephone));
+        }
+
+        clients.add(new Client(id,nom,prenom,email,adresse,telephone));
+        System.out.println("CLIENTS AJOUTER AVEC SUCCES");
+    }
+
+
+
+    public void menuclient(){
+        System.out.print("------MENU CLIENT-------\n"+
+                "1-ajouter\n"+
+                "2-modifier\n"+
+                "3-supprimer\n"+
+                "4-afficher\n"+
+                "----- ENTREZ VOTRE CHOIX :");
+
+        int choix=scanner.nextInt();
+
+        switch (choix){
+            case 1:
+                ajouterclient();
+                break;
+            default:
+                System.out.println("choix invalide");
+                break;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", telephone=+212" + telephone +
+                '}';
     }
 }
